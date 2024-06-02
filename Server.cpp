7830086bbd4be64888e8c63d419aa83308233524
nadir elmakias <34961490+Nadir0702@users.noreply.github.com>
2024-06-02 15:49:47 +0300

    Add files via upload
    first commit
    creating 4 miner threads and using original thread as server thread

---
+++ Server.cpp
@@ -0,0 +1,11 @@
+#include "Server.hpp"
+
+void Server::CreateGenesisBlock()
+{
+    g_SuggestedBlock = { 0, 0, 0, 0, 0, 0, 0 };
+}
+
+void Server::ManageBlockChain()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "Server.hpp"
+#include "Miner.hpp"
+#include "Utils.hpp"
+
+BLOCK_T g_SuggestedBlock;
+BLOCK_T g_BlockChainHead;
+
+int main()
+{
+    Server server;
+    Miner miners[4];
+    pthread_t minerThreadsIDs[4];
+    void* res;
+
+    server.CreateGenesisBlock();
+    server.ManageBlockChain();
+    for (size_t i = 0; i < 4; i++)
+    {
+        miners[i].SetMinerID(i + 1);
+        pthread_create(&minerThreadsIDs[i], NULL, Miner::MinerFlow, &miners[i]);
+    }
+    
+
+    
+    for (size_t i = 0; i < 4; i++)
+    {
+        pthread_join(minerThreadsIDs[i], &res);  
+    }
+
+    return 0;
+}
